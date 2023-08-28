b827ae8a9aa830acb3174163c94772849e32e34c
Vadim Cherepanov <xenos26@gmail.com>
2023-08-28 22:16:32 +0700

    feat: add response from server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { connectToDb, getDb } = require("./db.js")
+const { connectToDb, getDb } = require("./db.js");
 
 const PORT = 3000;
 
@@ -17,3 +17,21 @@ connectToDb((err) => {
   }
 })
 
+app.get("/movies", (req, res) => {
+  const movies = [];
+  db
+    .collection("movies")
+    .find()
+    .sort({ title: 1 })
+    .forEach((movie) => movies.push(movie))
+    .then(() => {
+      res
+        .status(200)
+        .json(movies)
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Something goes wrong..." })
+    })
+})
