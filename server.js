aa636647bc997ecc6137cf58939671a5bc648d95
Vadim Cherepanov <xenos26@gmail.com>
2023-08-28 21:55:02 +0700

    feat: add connectToDB function

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const { connectToDb, getDb } = require("./db.js")
 
 const PORT = 3000;
 
 const app = express();
+let db;
 
-app.listen(PORT, (err) => {
-  err ? console.error(err) : console.log(`Listening port ${PORT}`)
+connectToDb((err) => {
+  if (!err) {
+    app.listen(PORT, (err) => {
+      err ? console.error(err) : console.log(`Listening port ${PORT}`)
+    })
+    db = getDb();
+  } else {
+    console.error(`DB connection error: ${err}`)
+  }
 })
+
